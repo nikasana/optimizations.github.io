43669f58c1b50c3efd2c29c4d342ede9b70ffd5d
AniJojua <ani.jojua@yahoo.com>
2017-05-28 17:02:14 +0400

    Update send_email.php

--- temp/send_email.php
+++ temp/send_email.php
@@ -19,8 +19,7 @@ if(isset($_POST['email'])) {
     if(!isset($_POST['first_name']) ||
         !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
-        !isset($_POST['comments'])) {
+        !isset($_POST['text'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
@@ -29,8 +28,7 @@ if(isset($_POST['email'])) {
     $first_name = $_POST['first_name']; // required
     $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
-    $comments = $_POST['comments']; // required
+    $text = $_POST['text']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -49,8 +47,8 @@ if(isset($_POST['email'])) {
     $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
   }
  
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  if(strlen($text) < 2) {
+    $error_message .= 'The text you entered do not appear to be valid.<br />';
   }
  
   if(strlen($error_message) > 0) {
@@ -70,8 +68,8 @@ if(isset($_POST['email'])) {
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+
+    $email_message .= "Text: ".clean_string($text)."\n";
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".

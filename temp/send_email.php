f0d30be32f2785d50300c885f56e875182b6e355
AniJojua <ani.jojua@yahoo.com>
2017-05-28 17:05:10 +0400

    Update send_email.php

--- temp/send_email.php
+++ temp/send_email.php
@@ -2,13 +2,12 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    $email_to = "nikoloz.sanakoevi@gmail.com";
+    $email_subject = "rawrawrwar";
  
     function died($error) {
         // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
+        echo "შეცდომა! ";
         echo $error."<br /><br />";
         echo "Please go back and fix these errors.<br /><br />";
         die();
@@ -20,7 +19,7 @@ if(isset($_POST['email'])) {
         !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
         !isset($_POST['text'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        died('rip');       
     }
  
      
@@ -34,21 +33,21 @@ if(isset($_POST['email'])) {
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
   if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    $error_message .= 'აკრიფე ნორმალური ემაილი პლზ<br />';
   }
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
   if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+    $error_message .= 'ეგ რა სახელია<br />';
   }
  
   if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+    $error_message .= ';დდდდდდდდდდდდდდ<br />';
   }
  
   if(strlen($text) < 2) {
-    $error_message .= 'The text you entered do not appear to be valid.<br />';
+    $error_message .= 'ხხხხხხხხხხხხხხხხხხხხხხხ<br />';
   }
  
   if(strlen($error_message) > 0) {
@@ -80,7 +79,7 @@ $headers = 'From: '.$email_from."\r\n".
  
 <!-- include your own success html here -->
  
-Thank you for contacting us. We will be in touch with you very soon.
+გაიგზავნა.
  
 <?php
  
